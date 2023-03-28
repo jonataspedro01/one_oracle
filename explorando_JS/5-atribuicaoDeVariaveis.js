3fdb021c893f84f5563fede4872f02084a35fdb2
jonataspedro02 <p.jonatas@ifrn.edu.br>
2023-03-28 09:05:14 -0300

    Explorando JS

---
+++ explorando_JS/5-atribuicaoDeVariaveis.js
@@ -0,0 +1,16 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+const idade = 29;
+const primeiroNome = "Jonatas";
+const sobrenome = "Pedro";
+
+console.log(primeiroNome, sobrenome)
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
+let idadeVariada; //declarando variável
+idadeVariada = 26; //atribuindo valor
+idadeVariada = idadeVariada +1 ;
+console.log(idadeVariada);
---
+++ explorando_JS/6-lista.js
@@ -0,0 +1,15 @@
+//console.log(`Trabalhando com listas`);
+// const salvador = `Salvador`;
+// const natal = `Natal`;
+// const canguaretama = `Canguaretama`;
+// Crtl + k + c = para comentar código
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `Natal`,
+    `Canguaretama`
+);
+
+listaDeDestinos.push(`Curitiba`);
+console.log("Destinos possíveis: ");
+console.log(listaDeDestinos);
