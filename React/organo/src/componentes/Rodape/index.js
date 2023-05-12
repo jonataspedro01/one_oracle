affcd6d310027cd0689aac302905cabdd2ce8b63
jonataspedro02 <p.jonatas@ifrn.edu.br>
2023-05-12 18:53:20 -0300

    Curso de React finalizado

---
+++ React/organo/src/componentes/Rodape/index.js
@@ -0,0 +1,15 @@
+import './rodape.css'
+
+function Rodape () {
+    return (
+        <footer>
+            <div className="dev">
+                <p class="antisocial">Desenvolvido por : Jonatas Pedro</p>
+                <a class="footer-link" href="https://www.linkedin.com/in/jonataspedrodev" target="_target">Linkedin | </a>
+                <a class="footer-link" href="https://github.com/jonataspedro01" target="_target"> Github</a>       
+            </div>
+        </footer>
+    )
+}
+
+export default Rodape
